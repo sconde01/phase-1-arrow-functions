c4270362dd647838075bdb90ceec6e0815ef71e2
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-06-15 07:49:24 -0700

    commit tree 36b83b36238b0e952772099223d76455321f776a

---
+++ index.js
@@ -0,0 +1,10 @@
+//need three functions divide
+const divide = () => 2000 / 100;
+
+
+//function called square
+const square = num3 => num3**2;
+
+
+//funtion called add
+const add = (num4, num5) => num4+num5;
